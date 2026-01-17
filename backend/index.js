af2a4963b0581c1fee99a6acf7c3d019f2a44617
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-17 21:53:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import userAuth from './routes/user/userAuth.js';
 import doctorAuth from './routes/doctor/doctorAuth.js';
+import { connectDB } from './config/connectDB.js';
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cors({
 }));
 
 const port = process.env.PORT || 5000:
+
+connectDb();
 
 app.use(userAuth);
 app.use(doctorAuth);
